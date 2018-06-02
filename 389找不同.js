0fbdb34198d17b3731ee921c54fb73389cb181ab
Buddhas <2424932515@qq.com>
2018-06-02 12:54:03 +0800

    找不同

---
+++ 389找不同.js
@@ -0,0 +1,14 @@
+var findTheDifference = function(s, t) {
+    var s = s.split("").sort().join("")
+    var t = t.split("").sort().join("")
+    for(var i = 0; i < s.length; i++){
+        if(s[i] !== t[i]){
+            return t[i]
+        }
+    }
+    if(i === t.length){
+        return t[i]
+    }
+};
+console.log(findTheDifference("acbd",
+"abcde"))
--- text.js
+++ text.js
@@ -1,2 +1,22 @@
-var obj = {}
-obj.push
+var str = "bbaadefscdf"
+
+/*var change = function(str){
+    str.split().sort((m,n) =>{
+        var first = m.charCodeAt()
+        var second = n.charCodeAt()
+        if(first <= second){
+            return - 1
+        }
+        if(first > second){
+            return 1
+        }
+    }
+
+console.log(change(str))*/
+
+var change = function(str){
+    var str1 = str.split("").sort().join("")
+    return str1
+}
+
+console.log(change(str))

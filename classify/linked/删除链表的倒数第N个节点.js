faf5fa3f39df815616b061154b33927bc321de61
chenbin01 <chenbin01@zhuanzhuan.com>
2021-01-15 15:14:47 +0800

    feat:删除倒数第N个节点

--- classify/linked/删除链表的倒数第N个节点.js
+++ classify/linked/删除链表的倒数第N个节点.js
@@ -2,3 +2,24 @@ function Linked(value) {
   this.next = null
   this.value = value
 }
+
+
+var removeNthFromEnd = function(head, n) {
+  let fast = head
+  let slow = head
+  let pre = slow
+
+  while(n > 0) {
+    fast = fast.next
+    n--
+  }
+  if (!first) return head.next;
+  while(fast) {
+    pre = slow
+    fast = fast.next
+    slow = slow.next
+  }
+  pre.next = slow.next
+  return head
+};
+
